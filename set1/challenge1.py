b9623a42ff8f3e93ccc0412e869cf8253fec4218
amritgiri <amritgiri5813@gmail.com>
2024-07-02 16:58:00 +0545

    Add input and output string

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -1,6 +1,9 @@
 """
 HEX to BASE64 convert
 
+String=[49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d]
+output=[SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t]
+
 @input hex string from the user
 
 @returned by hex2base64: Byte encode of base64
